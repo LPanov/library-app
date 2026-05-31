e8873140f3071356b925e18c9591b23cea9c39e3
LPanov <lubomirpanov99@gmail.com>
2026-05-31 10:50:53 +0300

    fix: improve SecurityConfiguration class

---
+++ api-gateway/src/main/java/com/library/app/apigateway/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.library.app.apigateway.config;
+
+public class JwtConstant {
+    public static final String JWT_HEADER = "Authorization";
+    public static final String SECRET_KEY = "kaflhsladsfnlacuiladvakadcli";
+}
--- api-gateway/src/main/java/com/library/app/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/library/app/apigateway/config/SecurityConfig.java
@@ -1,12 +1,22 @@
 package com.library.app.apigateway.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.constraints.NotNull;
+import org.jspecify.annotations.Nullable;
 import org.springframework.boot.security.autoconfigure.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 public class SecurityConfig {
@@ -23,11 +33,35 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
-        return httpSecurity.authorizeHttpRequests(authorize -> authorize
+        return httpSecurity
+                .sessionManagement(management -> management.sessionCreationPolicy(
+                        SessionCreationPolicy.STATELESS
+                ))
+                .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(freeResourceUrls).permitAll()
+                        .requestMatchers("/api/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated())
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .addFilterBefore(new JwtValidator(), BasicAuthenticationFilter.class)
+                .csrf(AbstractHttpConfigurer::disable)
+//                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .build();
+    }
+
+    private CorsConfigurationSource corsConfigurationSource() {
+        return new CorsConfigurationSource() {
+
+            @Override
+            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                CorsConfiguration cfg = new CorsConfiguration();
+                cfg.setAllowCredentials(true);
+                cfg.setAllowedOrigins(Arrays.asList("http://localhost:5173/"));
+                cfg.setAllowedMethods(Collections.singletonList("*"));
+                cfg.setExposedHeaders(Collections.singletonList("Authorization"));
+                cfg.setMaxAge(360L);
+                return cfg;
+            }
+        };
     }
 
 }
