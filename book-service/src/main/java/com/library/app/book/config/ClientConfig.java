aa9a38f49241a2132c806abdbfd440192c7f9457
LPanov <lubomirpanov99@gmail.com>
2026-06-23 21:36:25 +0300

    feat: configure OAuth2 authentication

--- book-service/src/main/java/com/library/app/book/config/ClientConfig.java
+++ book-service/src/main/java/com/library/app/book/config/ClientConfig.java
@@ -4,6 +4,8 @@ import com.library.app.book.client.GenreClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.client.OAuth2ClientHttpRequestInterceptor;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -15,9 +17,13 @@ public class ClientConfig {
     private String genreServiceUrl;
 
     @Bean
-    public GenreClient genreClient() {
+    public GenreClient genreClient(OAuth2AuthorizedClientManager authorizedClientManager) {
+        OAuth2ClientHttpRequestInterceptor oauth2Interceptor =
+                new OAuth2ClientHttpRequestInterceptor(authorizedClientManager);
+
         RestClient restClient = RestClient.builder()
                 .baseUrl(genreServiceUrl)
+                .requestInterceptor(oauth2Interceptor)
                 .build();
 
         RestClientAdapter adapter = RestClientAdapter.create(restClient);
